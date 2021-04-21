c4c44bd16249116c736dafde50d2fc2b36a3749c
mankaguz <81649315+mankaguz@users.noreply.github.com>
2021-04-21 18:52:02 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -7,18 +7,18 @@ router.get('/', (req, res) => {
     res.sendFile(__path + '/views/home.html')
 })
 
-router.get('/index', (req, res) => {
+router.get('/index.php', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
-router.get('/index/about', (req, res) => {
+router.get('/index.php/about', (req, res) => {
     res.sendFile(__path + '/views/about.html')
 })
-router.get('/index/index1', (req, res) => {
+router.get('/index.php/index1', (req, res) => {
     res.sendFile(__path + '/views/index1.html')
 })
 
-router.get('/index/news', (req, res) => {
+router.get('/index.php/news', (req, res) => {
     res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
 })
 
